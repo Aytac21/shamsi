bd888608f0823de5fd03f591b406a31a32565ec5
NicatDev <nicat254memmedov@gmail.com>
2025-06-09 21:16:45 +0400

    mian

--- gallery/urls.py
+++ gallery/urls.py
@@ -5,7 +5,7 @@ app_name = 'gallery'
 
 urlpatterns = [
     path('gallery/', views.gallery_view, name='gallery'),
-    path('videos/', views.gallery_video_view, name='gallery'),
+    path('videos/', views.gallery_video_view, name='video-gallery'),
     path('load-more-images/', views.load_more_images, name='load_more_images'),
     path('ajax/load-more-videos/', views.load_more_videos, name='load_more_videos'),
 ]
